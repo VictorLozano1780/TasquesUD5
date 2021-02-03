90f81c4c6ece12771c6c986752807d6584c9a5b6
Víctor <victor.lozano1780@gmail.com>
2021-02-03 15:22:17 +0100

    Ejercicio 1

---
+++ TasquesUD5_1/src/TasquesUD5_1.java
@@ -0,0 +1,27 @@
+/**
+ * 
+ */
+
+/**
+ * @author Víctor Lozano
+ *
+ */
+public class TasquesUD5_1 {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		double num1 = 3.2, num2 = 4.5;
+		
+		if (num1 > num2) {
+			System.out.println(num1 + " es mas grande");
+		} else if (num2 > num1) {
+			System.out.println(num2 + " es mas grande");
+		} else {
+			System.out.println("Son iguales");
+		}
+	}
+
+}
